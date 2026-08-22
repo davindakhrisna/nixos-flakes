b577283ecb44a23b71f6a699c31a7a882d6f89bf
davindakhrisna <arpeggio.gns@gmail.com>
2026-08-22 18:02:05 +0700

    SHA

--- modules/home/gui/thorium/default.nix
+++ modules/home/gui/thorium/default.nix
@@ -9,7 +9,7 @@
 
   src = pkgs.fetchurl {
     url = "https://github.com/edrlab/thorium-reader/releases/download/v${version}/Thorium-${version}.AppImage";
-    hash = "sha256-uMdfEwgRj/ggTvwhYt+B93LrkeD4bmBliWY12V10qaA=";
+    hash = "sha256-wMdfEwgRj/ggTvwhYt+B93LrkeD4bmBliWY12V10qaA=";
   };
 
   appimageContents = pkgs.appimageTools.extractType2 {
